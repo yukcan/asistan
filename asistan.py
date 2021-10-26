a22eb91bb0d3e1aad47118af26ed7c0def0b5100
yukcan <78096072+yukcan@users.noreply.github.com>
2021-10-26 21:53:53 +0300

    Update asistan.py

--- asistan.py
+++ asistan.py
@@ -58,11 +58,85 @@ def responce(voice):
             speak("euro "+soup+" olmuş neyseki maaşımı euroyla almıyorum")
         else:
             speak(selection)
+    if "bende iyiyim" in voice:
+        speak("Allah iyilik versin")
     if "görüşürüz" in voice:
         selection = ["görüşürüz yavrum","bi siktir git artık","sonunda","görüşmek üzere"]
         selection = random.choice(selection)
         speak(selection)
         exit()
+    if "tarih" in voice:
+        today = time.strftime("%A")
+        today = today.lower()
+        if today == "monday":
+            today = "pazartesi"
+        elif today == "tuesday":
+            today = "salı"
+        elif today == "wednesday":
+            today = "çarşamba"
+        elif today == "thursday":
+            today = "perşembe"
+        elif today == "friday":
+            today = "cuma"
+        elif today == "saturday":
+            today = "cumartesi"
+        else:
+            today = "pazar"
+        month = time.strftime("%B")
+        if month == "january":
+            month = "ocak"
+        elif month == "february":
+            month = "şubat"
+        elif month == "march":
+            month = "mart"
+        elif month == "april":
+            month = "nisan"
+        elif month == "may":
+            month = "mayıs"
+        elif month == "june":
+            month = "haziran"
+        elif month == "july":
+            month = "temmuz"
+        elif month == "august":
+            month = "ağustos"
+        elif month == "september":
+            month = "eylül"
+        elif month == "october":
+            month = "ekim"
+        elif month == "november":
+            month = "kasım"
+        else:
+            month = "aralık"
+        speak("tarih",today,month,time.strftime("%Y"))
+    if "hangi yıldayız" in voice:
+        speak(time.strftime("%Y")+" yılındayız")
+    if "hangi aydayız" in voice:
+        month = time.strftime("%B")
+        if month == "january":
+            month = "ocak"
+        elif month == "february":
+            month = "şubat"
+        elif month == "march":
+            month = "mart"
+        elif month == "april":
+            month = "nisan"
+        elif month == "may":
+            month = "mayıs"
+        elif month == "june":
+            month = "haziran"
+        elif month == "july":
+            month = "temmuz"
+        elif month == "august":
+            month = "ağustos"
+        elif month == "september":
+            month = "eylül"
+        elif month == "october":
+            month = "ekim"
+        elif month == "november":
+            month = "kasım"
+        else:
+            month = "aralık"
+        speak(month+" ayındayız")
     if "hangi gündeyiz" in voice:
         today = time.strftime("%A")
         today = today.lower()
@@ -101,9 +175,22 @@ def responce(voice):
                 break
 
 if wakeUp:
-    selection = ["sana nasıl yardımcı olabilirim","size nasıl yardım edebilirim","gene ne var","beş dakika uyutmadınız dır dır dır ne var söyle","merhaba"]
+    selection = ["saat","sana nasıl yardımcı olabilirim","size nasıl yardım edebilirim","gene ne var","beş dakika uyutmadınız dır dır dır ne var söyle","merhaba"]
     selection = random.choice(selection)
-    speak(selection)
+    if selection == "saat":
+        clock = time.strftime("%H")
+        if clock<11:
+            selection = ["günaydın bugün sana nasıl yardımcı olabilirim","bu saatte mesaiye mi çağırılır","günaydın","sabah sabah beni neden uyandırdın","daha kargalar sikini duvara sürtmediler beni neden uyandırdın"]
+            selection = random.choice(selection)
+            speak(selection)
+        elif clock<16:
+            speak("tünaydın sana nasıl yardım edebilirim")
+        else:
+            selection = ["iyi akşamlar","iyi akşamlar hayırdır bu saatte","bu saate mesaiyede çağrılmaz ki","akşam akşam beni neden rahatsız ettin"]
+            selection = random.choice(selection)
+            speak(selection)
+    else:
+        speak(selection)
     while True:
         voice = record()
         if voice != "":
